59fd7d181ec8778cd5479ca8fbbab2394c19d108
danielshindi <danielshindi@hotmail.com>
2023-07-08 19:04:38 -0300

    Commit atualizando o Projeto Sistema Bancário para versão 2

--- README.md
+++ README.md
@@ -1,30 +1,42 @@
-# Projeto de sistema bancario
+# Projeto de sistema bancário
 
-O objetivo é implementar três operações essenciais: depósito, saque e extrato, para apenas um usuário, sem se preocupar com identificar qual é o número da  agência e conta bancária. O sistema será desenvolvido, em Python, para um banco que busca monetizar suas operações.
+O objetivo é fazer uma segunda versão do Sistema Bancário (https://github.com/danielshindi/curso-python-DIO-sistema_bancario) e implementar as três operações essenciais: depósito, saque e extrato, em funções.
+
+Criar duas novas funções: cadastrar usuário (cliente) e cadastrar conta bancária.
 
 
 
+### Função de depósito
 
+A função depósito deve receber os argumentos apenas por posição (positional only).
 
-### Operação de depósito
+Sugestão de argumentos: saldo, valor, extrato.
+
+Sugestão de retorno: saldo e extrato.
 
 Deve ser possível depositar valores positivos para a conta bancária.
 
-Os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
 
 
+### Função de saque
 
-### Operação de saque
+A função saque deve receber os argumentos apenas por nome (keyword only).
+
+Sugestão de argumentos: saldo, valor, extrato, limite, numero_saques, limite_saques.
+
+Sugestão de retorno: saldo e extrato.
 
 O sistema deve permitir realizar 3 saques diários com limite máximo de R$500,00 por saque.
 
-Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por faltaa de saldo.
-
-Todos os saques devem ser armazenados em uma variavel e exibidos na opreção de extrato.
+Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo.
 
 
 
-### Operação de extrato
+### Função de extrato
+
+A função extrato deve receber os argumentos por posição e nome (positional e keyword).
+
+Argumentos posicionais: saldo, argumentos nomeados: extrato.
 
 Essa operação deve listar todos os depósitos e saques realizados na conta.
 
@@ -33,6 +45,44 @@ No fim da listagem deve ser exibido o saldo atual da conta.
 Se o extrato estiver em branco, exibir a mensagem: "Não foram realizadas movimentações".
 
 Os valores devem ser exibidos utilizando o formato R$xxx.xx, exemplo: 1500.45 = R$1500.45
+
+
+
+### Novas funções
+
+#### Criar usuário (cliente)
+
+O programa deve armazenar os usuários em uma lista.
+
+Um usuário é composto por: nome, data de nascimento, cpf e endereço.
+
+O endereço é uma string com o formato: logradouro, número - bairro - cidade/sigla estado.
+
+Deve ser armazenado somente os números do CPF (string).
+
+Não podemos cadastrar 2 usuários com o mesmo CPF.
+
+
+
+#### Cadastrar conta corrente
+
+O programa deve armazenar contas em uma lista.
+
+Uma conta é composta por: agência, número da conta e usuário.
+
+O número da conta é sequencial, iniciando em 1.
+
+O número da agência é fixo: '0001'.
+
+O usuário pode ter mais de uma conta, mas uma conta pertence a somente um usuário.
+
+
+
+##### Dica
+
+- Para vincular um usuário a uma conta, filtre a lista de usuários buscando o número do CPF para cada usuário da lista.
+
+
 
 
 
--- sistema_bancario.py
+++ sistema_bancario.py
@@ -2,15 +2,22 @@ depositos = 0
 saques = 0
 QUANTIDADE_SAQUE_DIARIO = 3
 VALOR_SAQUE_MAX = 500
+AGENCIA = '0001'
 quantidade_saques = 0
 saldo = 0
 extrato = ''
+usuarios = []
+contas = []
+numero_conta = 1
 
 menu = '''------------------- Selecione uma opção: ----------------------
 
     d - Depositar
     s - Sacar
     e - Extrato
+    u - Cadastrar usuário
+    c - Cadastrar conta corrente
+    l - Listar contas
     q - Sair
 
 ----------------------------------------------------------------
@@ -29,24 +36,83 @@ def validaFloat(mensagem):
         except ValueError:
             print('Valor inválido.')
             continue
-    return valor    
+    return valor
 
-def depositar(valor):
-    global saldo, extrato
+def depositar(valor, saldo, extrato):
     saldo += valor
     extrato += f'+ R${valor:.2f}\n'
+    return saldo, extrato
 
-def sacar(valor):
-    global saldo, extrato
+def sacar(*, valor, saldo, extrato):
     saldo -= valor
     extrato += f'- R${valor:.2f}\n'
+    return saldo, extrato
+
+def emitir_extrato(saldo, *, extrato):
+    print('==========================Extrato: ============================')
+    print(extrato if extrato != '' else 'Nenhuma movimentação realizada', f'\nSaldo: R${saldo:.2f}')
+    print('===============================================================')
+
+def busca_cpf(cpf):
+    for usuario in usuarios:
+        if usuario['cpf'] == cpf:
+            return usuario
+    return None
+
+def cadastrar_usuario():
+    cpf = input('Digite seu CPF (apenas números): ')
+    if busca_cpf(cpf):
+        print('Usuário já cadastrado!')
+    else:
+        nome = input('Digite seu nome completo: ')
+        nascimento = input('Digite sua data de nascimento: ')
+        print('Digite seu endereço:')
+        logradouro = input('Logradouro: ')
+        numero_endereco = input('Número: ')
+        bairro = input('Bairro: ')
+        cidade_estado = input('Cidade/Estado: ')
+
+        usuario = {"cpf": cpf,
+                "nome": nome,
+                "nascimento": nascimento,
+                "endereco": {"logradouro": logradouro,
+                            "número": numero_endereco, 
+                            "bairro": bairro, 
+                            "cidade/estado": cidade_estado
+                            }
+                    }
+        usuarios.append(usuario)
+    
+def criar_conta(contas):
+    cpf = input('Digite o CPF do usuário: ')
+    usuario = busca_cpf(cpf)
+    if not usuario:
+        print('Usuário não cadastrado!')
+        return None
+    else:
+        conta = {'agencia': AGENCIA,
+                'numero_conta': numero_conta,
+                'usuario': usuario}
+        print('Conta criada com sucesso!')
+        return conta
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(linha)
 
 
+#---------------------------------------------- Main ----------------------------------------------------
 while True:
     opcao = input(menu)
     if opcao in 'Dd':
         valor = validaFloat('Digite o valor do depósito: ')
-        depositar(valor)
+        saldo, extrato = depositar(valor, saldo, extrato)
 
     elif opcao in 'Ss':
         if quantidade_saques >= QUANTIDADE_SAQUE_DIARIO:
@@ -55,7 +121,7 @@ while True:
             valor = validaFloat('Digite o valor do saque: ')
             if valor <= VALOR_SAQUE_MAX:
                 if valor <= saldo:
-                    sacar(valor)
+                    saldo, extrato = sacar(valor=valor, saldo=saldo, extrato=extrato)
                     quantidade_saques += 1
                 else:
                     print('\nSaldo insuficiente.\n')
@@ -63,17 +129,18 @@ while True:
                 print(f'\nValor máximo para saque é de R${VALOR_SAQUE_MAX:.2f}.\n')
 
     elif opcao in 'Ee':
-        print('==========================Extrato: ============================')
-        print(extrato if extrato != '' else 'Nenhuma movimentação realizada', f'\nSaldo: R${saldo:.2f}')
-        print('===============================================================')
+        emitir_extrato(saldo, extrato=extrato)
     
     elif opcao in 'Qq':
         break
-
+    elif opcao in 'Uu':
+        cadastrar_usuario()
+    elif opcao in 'Cc':
+        conta = criar_conta(contas)
+        if conta:
+            contas.append(conta)
+            numero_conta += 1    
+    elif opcao in 'Ll':
+        listar_contas(contas)
     else:
         print('Opção inválida.')
-
-            
-            
-                
-
